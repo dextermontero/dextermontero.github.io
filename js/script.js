1283807a2388f70849c6f38afa7994f071182b8a
[Dev]Dexter <dexter.montero09@gmail.com>
2022-10-01 19:01:54 +0800

    Code CleanUP: remove unused script

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ const menuList = document.querySelector('#menuList');
 menuOpt.addEventListener('click', () => {
     if(menuList.classList.contains('hidden')){
         menuList.classList.remove('hidden');
-        navbar.classList.add('');
     }else{
         menuList.classList.add('hidden');
     }
